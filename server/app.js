5ab241a38d9dda59d8c2ba1118396a625060b021
Tanner <tannercdolby@gmail.com>
2021-03-28 00:37:42 -0700

    Netlify functions working, app is live

--- server/app.js
+++ server/app.js
@@ -11,14 +11,9 @@ const assetsDir = "./node-app/dist/assets";
 const slugify = require("./_helpers/slugify");
 // const env = require("/blog-client/app/src/environments/environment");
 
-var corsOptions = {
-    origin: "http://localhost:4200"
-}
-
 app.use(cors({
     optionsSuccessStatus: 200,
     origin: "http://localhost:9000"
-    // origin: "https://modest-bhabha-3a9de8.netlify.app"
 }));
 app.options("*", cors());
 
@@ -28,9 +23,9 @@ app.use(bodyParser.json());
 // specify the mount path for the static directory `/dist`
 app.use("/", express.static("dist"));
 
-const postRouter = require("../routes/post");
-const postsRouter = require("../routes/posts");
-const getPostByTagRouter = require("../routes/get-post-by-tag");
+const postRouter = require("./routes/post");
+const postsRouter = require("./routes/posts");
+const getPostByTagRouter = require("./routes/get-post-by-tag");
 
 // Set Netlify function routes
 app.use("/.netlify/functions/app", postRouter);
