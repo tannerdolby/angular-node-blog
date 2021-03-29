3d61d14c42487292d72d2c1e233398ee0d47e751
Tanner <tannercdolby@gmail.com>
2021-03-28 22:47:10 -0700

    build minutes depleting

--- functions/rawr.js
+++ functions/rawr.js
@@ -7,10 +7,11 @@ const resolved = process.env.LAMBDA_TASK_ROOT ? path.resolve(process.env.LAMBDA_
 exports.handler = async (event, context) => {
     try {
         const f = await fsp.readFile(`${dir}${resolved}`, { encoding: "utf8" });
+        //f = f.toString();
         console.log(f);
         return {
             statusCode: 200,
-            body: JSON.stringify({ f: JSON.stringify(f) })
+            body: JSON.stringify({ f: JSON.parse(f)})
         };
     } catch (e) {
         return {
