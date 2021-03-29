ef0dd3932d438a351c23bed87d3b7b9b4f8ed665
Tanner <tannercdolby@gmail.com>
2021-03-29 11:41:12 -0700

    restructure

--- node-app/server/routes/test.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs").promises;
-const path = require("path");
-
-exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ msg: "Hello World!" })
-    };
-};
