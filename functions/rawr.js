8aa4eaf37141a68faae4d7a0670b63aaf243aea3
Tanner <tannercdolby@gmail.com>
2021-03-28 23:00:52 -0700

    zzzz

--- functions/rawr.js
+++ functions/rawr.js
@@ -5,18 +5,21 @@ const file = "./blog.json";
 const resolved = process.env.LAMBDA_TASK_ROOT ? path.resolve(process.env.LAMBDA_TASK_ROOT, file) : path.resolve(__dirname, file);
 
 exports.handler = async (event, context) => {
-    try {
-        const f = await fsp.readFile(`${dir}${resolved}`, { encoding: "utf8" });
-        //f = f.toString();
-        console.log(f);
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ f: JSON.parse(f)})
-        };
-    } catch (e) {
-        return {
-            statusCode: 500,
-            body: e
-        };
-    }
+        try {
+            const f = await fsp.readFile(`${dir}${resolved}`, { encoding: "utf8" });
+            //f = f.toString();
+            console.log(f);
+            return {
+                statusCode: 200,
+                body: f
+            }
+        } catch (e) {
+            return {
+                statusCode: 500,
+                body: e
+            };
+        }
+    // if (event.httpMethod == "GET") {
+    //     get();
+    // }
 };
