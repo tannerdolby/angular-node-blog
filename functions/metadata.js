f8a2d84dab311fa6b056e0f1de29da855f5b696f
Tanner <tannercdolby@gmail.com>
2021-03-31 15:18:15 -0700

    Fresh build, attempt to fix unmarshalling lambda error

--- functions/metadata.js
+++ functions/metadata.js
@@ -25,7 +25,7 @@ exports.handler = async (event, context) => {
             });
             return {
                 statusCode: 200,
-                body: data
+                body: JSON.stringify({ data: data })
             }
         })
         .catch(error => ({
